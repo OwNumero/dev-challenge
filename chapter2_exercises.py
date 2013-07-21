933d19fd7a8a48eeb561bc4003767aa23f2ffa49
OwNumero <ow.numero@gmail.com>
2013-07-21 03:14:34 -0400

    OwNumero uploaded Chapted2_excercises.py

--- chapter2_exercises.py
+++ chapter2_exercises.py
@@ -1,3 +1,91 @@
 # Exercises for chapter 2:
 
-# Name:
+# Name:# 10 day Dev Challenge
+# by OwNumero
+# 
+# 
+# Exercice 2.1: 
+#
+# when the number entered starts with a zero
+# Python interprets it as a number base 8
+# using numbers 0 through 7
+# 
+# 0 is 0
+# 01 is 1
+# 04 is 4
+# 07 is 7
+# 10 is 8
+# 11 is 9
+# note 08, 09 don't exist because they use 
+# numbers after 7, which is the highest in
+# base 8
+#
+# Exercise 2.2
+#
+# run the sequence as a script
+# 5
+# x = 5
+# x + 1
+# 6
+#
+# print each expression
+# print 5
+# 5
+# print x + 1
+# 6
+#
+# Note that the book hasn't explained how to save or run a script yet
+#
+#
+# Exercise 2.3
+#
+# width = 17
+# height = 12.0
+# delimiter = ' . '
+# 1. width/2
+#    8
+#    <type 'int'>
+# 2. width/2.0
+#    8.5
+#    <type 'float'>
+# 3. height/3
+#    4
+#    <type 'float'>
+# 4. 1 + 2 * 5
+#    11
+#    <type 'int'>
+# 5. delimiter * 5
+#    ' .  .  .  .  . '
+#    <type 'str'>
+#
+#
+# Exercise 2.4
+# 
+# 1.
+# r=5
+# pi=3.14159
+# r**3*pi*4/3
+# 523..5983333333332
+#
+# 2.
+# bp=24.95
+# d=0.6
+# sh1=3
+# shs=0.75
+# nbr=6=
+# print bp*d+sh1+(nbr-1)*shs
+# 62.22
+#
+# 3.
+# dtime=6*60+52.0
+# dist1=1.0
+# speed1=(8+15/60)
+# dist2=3.0
+# speed2=(7+12/60)
+# atime=(dtime+dist1*speed1+dist2+speed2)/60
+# print atime
+# 7.35
+# (atime-7)*60
+# 20.999
+#
+# So the arrival time is 7:21:00
